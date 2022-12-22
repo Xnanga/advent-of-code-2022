7b3cadce20dfd8b34eb1310b67c5c58805aa6655
Xnanga <jpeutherer1@hotmail.co.uk>
2022-12-22 11:55:45 +0000

    Day Two solution implemented for part one

--- src/main/scala/com/github/xnanga/aoc_22/DayTwo.scala
+++ src/main/scala/com/github/xnanga/aoc_22/DayTwo.scala
@@ -1,31 +1,32 @@
 package com.github.xnanga.aoc_22
 
+import scala.collection.immutable.HashMap
 import scala.io.Source.fromFile
 
 object DayTwo extends App {
-  val allInputs = fromFile("src/main/resources/DayTwoInput.txt").getLines.toList
+  private val LOSING_POINTS = 0
+  private val DRAW_POINTS = 3
+  private val WINNING_POINTS = 6
 
-  val losingPoints = 0
-  val drawPoints = 3
-  val winningPoints = 6
+  private val allInputs = fromFile("src/main/resources/DayTwoInput.txt").getLines.toList
 
-  // A & X = Rock
-  // B & Y = Paper
-  // C & Z = Scissors
+  private def parseInput(input: String): List[Char] = {
+    val inputNoWhitespace = input.filterNot(_.isWhitespace)
+    List(inputNoWhitespace(0), inputNoWhitespace(1))
+  }
 
-  def determineChoice(letter: String): String = {
+  private val allParsedInputs = allInputs.toList.map(input => parseInput(input))
+
+  private def determineChoice(letter: Char): String = {
     letter match {
-      case "A" || "X" => "rock"
-      case "X" || "X" => "rock"
-      case "B" || "Y" => "paper"
-      case "B" || "Y" => "paper"
-      case "C" || "Z" => "scissors"
-      case "C" || "Z" => "scissors"
+      case ('A' | 'X') => "rock"
+      case ('B' | 'Y') => "paper"
+      case ('C' | 'Z') => "scissors"
       case _ => ""
     }
   }
 
-  def determineChoicePoints(choice: String): Int = {
+  private def determineChoicePoints(choice: String): Int = {
     choice match {
       case "rock" => 1
       case "paper" => 2
@@ -34,32 +35,64 @@ object DayTwo extends App {
     }
   }
 
-  def rockPaperScissorsMatch(choice: String, response: String): Map[String, Int] = {
-  val playerOneChoice = determineChoice(choice)
-  val playerTwoChoice = determineChoice(response)
-
-  var playerOnePoints = determineChoicePoints(playerOneChoice)
-  var playerTwoPoints = determineChoicePoints(playerTwoChoice)
-
-  if (playerOneChoice == playerTwoChoice) => {
-      playerOnePoints += 3
-      playerTwoPoints += 3
-    } else if (playerOneChoice == "rock" && playerTwoChoice == "scissors") => playerOnePoints += 6
-    else if (playerTwoChoice == "rock" && playerOneChoice == "scissors") => playerTwoPoints += 6
-    else if (playerOneChoice == "paper" && playerTwoChoice == "rock") => playerOnePoints += 6
-    else if (playerTwoChoice == "paper" && playerOneChoice == "rock") => playerTwoPoints += 6
-    else if (playerOneChoice == "scissors" && playerTwoChoice == "paper") => playerOnePoints += 6
-    else if (playerTwoChoice == "scissors" && playerOneChoice == "paper" => {
-      playerTwoPoints += 6
+  private def determineWinner(playerOneChoice: String, playerTwoChoice: String): String = {
+    if (playerOneChoice == playerTwoChoice) {
+      "draw"
+    } else if (playerOneChoice == "rock") {
+      if (playerTwoChoice == "paper") {
+        "playerTwo"
+      } else {
+        "playerOne"
+      }
+    } else if (playerOneChoice == "paper") {
+      if (playerTwoChoice == "scissors") {
+        "playerTwo"
+      } else {
+        "playerOne"
+      }
+    } else if (playerOneChoice == "scissors") {
+      if (playerTwoChoice == "rock") {
+        "playerTwo"
+      } else {
+        "playerOne"
+      }
+    } else {
+      "error with input(s)"
     }
   }
 
-  Map("playerOnePoints" -> playerOnePoints, "playerTwoPoints" -> playerTwoPoints)
+  private def calculateScores(inputList: List[Char]): List[Int] = {
+    val playerOneChoice = determineChoice(inputList(0))
+    val playerOneChoicePoints = determineChoicePoints(playerOneChoice)
+    val playerTwoChoice = determineChoice(inputList(1))
+    val playerTwoChoicePoints = determineChoicePoints(playerTwoChoice)
+    val matchOutcome = determineWinner(playerOneChoice, playerTwoChoice)
+
+    if (matchOutcome == "draw") {
+      List(playerOneChoicePoints + DRAW_POINTS, playerTwoChoicePoints + DRAW_POINTS)
+    } else if (matchOutcome == "playerOne") {
+      List(playerOneChoicePoints + WINNING_POINTS, playerTwoChoicePoints + LOSING_POINTS)
+    } else if (matchOutcome == "playerTwo") {
+      List(playerOneChoicePoints + LOSING_POINTS, playerTwoChoicePoints + WINNING_POINTS)
+    } else {
+      println("Error(s) with score inputs")
+      List(0, 0)
+    }
   }
 
-  def calculateScores(scoreList: List[String]): Unit = {
-    scoreList.foreach(score => println(score))
+  private def runStrategyGuide(inputList: List[List[Char]]): HashMap[String, Int] = {
+    var playerOneTotalScore = 0
+    var playerTwoTotalScore = 0
+
+    inputList.foreach(input => {
+      val outcomeScores = calculateScores(input)
+      playerOneTotalScore += outcomeScores(0)
+      playerTwoTotalScore += outcomeScores(1)
+    })
+
+    HashMap("playerOneScore" -> playerOneTotalScore, "playerTwoScore" -> playerTwoTotalScore)
   }
 
-  calculateScores(allInputs)
+  private val finalCalculatedScores = runStrategyGuide(allParsedInputs)
+  println(s"PlayerOneScore: ${finalCalculatedScores("playerOneScore")}\nPlayerTwoScore: ${finalCalculatedScores("playerTwoScore")}")
 }
